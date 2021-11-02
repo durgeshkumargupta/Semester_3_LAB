3fbfdd2c211a9f49be3474e21ec5755f2e9a182e
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-11-02 11:32:58 +0530

    Update DaoImp.java

--- JAVA LAB/Program-3/src/main/java/com/durgesh/daoImp/DaoImp.java
+++ JAVA LAB/Program-3/src/main/java/com/durgesh/daoImp/DaoImp.java
@@ -10,70 +10,57 @@ import org.hibernate.query.Query;
 import com.durgesh.hibernate.config.HibernateUtil;
 import com.durgesh.hibernate.model.Student;
 
-public class DaoImp 
-{
-	
-	public void getConnection()
-	{
-		 Transaction transaction = null;
-	        try(Session session = HibernateUtil.getSessionFactory().openSession()){
-	            transaction =  session.beginTransaction();
-	        }
-	            catch (Exception e) {
-					e.printStackTrace();
-				}
-	}
-	
-	public void insertData(String usn,String name,String address,String dept)
-	{
-		Student stu1=new Student(usn,name,address,dept);
-		
-        Transaction transaction = null;
-        try(Session session = HibernateUtil.getSessionFactory().openSession()){
-            transaction =  session.beginTransaction();
+public class DaoImp {
 
-            session.save(stu1);
-            transaction.commit();
-            System.out.println("Data Inserted");
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+	public void insertData(String usn, String name, String address, String dept) {
+		Student stu1 = new Student(usn, name, address, dept);
+
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+
+		session.save(stu1);
+		session.getTransaction().commit();
+		System.out.println("Data Inserted");
 
 	}
 
-	public void fetchData() 
-	{
-	
-		  //Create session factory object
-		  SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-		  //getting session object from session factory
-		  Session session = sessionFactory.openSession();
-		  //getting transaction object from session object
-		  session.beginTransaction();
-		  Query query = session.createQuery("from Student");
-		  List<Student> students = query.list();
-		  for(Student student : students)
-		  {
-		System.out.println("USN: "+student.getUsn()+", Student Name: "+student.getName()+", Department: "+student.getDept()+" Address: "+student.getAddress());
-		  }
-		  session.getTransaction().commit();
-		//  sessionFactory.close();
-		 }
-
-	public void deleteData() {
-		 Session session = null;
-	      Transaction transaction = null;
-		session = HibernateUtil.getSessionFactory().openSession();
-        transaction = session.getTransaction();
-        transaction.begin();
-
-        //Delete a persistent object
-        Student customer1=session.get(Student.class, 2L);
-        if(customer1!=null){
-           session.delete(customer1);
-           System.out.println("Student 1 is deleted");
-        }
-		
+	public void fetchData() {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from Student");
+		List<Student> students = query.list();
+		for (Student student : students) {
+			System.out.println("USN: " + student.getUsn() + ", Student Name: " + student.getName() + ", Department: "
+					+ student.getDept() + " Address: " + student.getAddress());
+		}
+		session.getTransaction().commit();
+		// sessionFactory.close();
 	}
-	
+
+	public void updateData(String name2, String usn2) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+
+		Student student = (Student) session.get(Student.class, usn2);
+		student.setName(name2);
+		System.out.println("Updated Successfully");
+		session.getTransaction().commit();
+		// sessionFactory.close();
+
+	}
+
+	public void deleteData(String usn3) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		Student student = (Student) session.load(Student.class, usn3);
+		session.delete(student);
+		System.out.println("Deleted Successfully");
+		session.getTransaction().commit();
+		// sessionFactory.close();
+	}
+
 }
