9536decddf9a4fbbbfee1f0bec26a341f7adad44
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-12-16 14:16:04 +0530

    Update HibernateUtil.java

--- JAVA LAB/Program-3(Second-way)/src/main/java/com/durgesh/HibernateUtil.java
+++ JAVA LAB/Program-3(Second-way)/src/main/java/com/durgesh/HibernateUtil.java
@@ -5,7 +5,8 @@ import java.util.Properties;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
-
+// See the database in college using this command
+// mysql -u 1rv20mc015 -p -h 1rv20mc015
 public class HibernateUtil 
 {
 	private static SessionFactory sessionFactory;
@@ -18,9 +19,9 @@ public class HibernateUtil
 		Properties properties=new Properties();
 		
 		properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-		properties.put(Environment.URL,"jdbc:mysql://localhost:3306/rvce");
-		properties.put(Environment.PASS, "root");
-		properties.put(Environment.USER,"root");
+		properties.put(Environment.URL,"jdbc:mysql://localhost:3306/rvce"); // "jdbc:mysql://172.16.34.105:3306/1rv20mc015"
+		properties.put(Environment.PASS, "root");                          // "1rv20mc015"
+		properties.put(Environment.USER,"root");                           // "1rv20mc015"
 		properties.put(Environment.DIALECT,"org.hibernate.dialect.MySQL5Dialect");
 		properties.put(Environment.SHOW_SQL, true);
 		properties.put(Environment.HBM2DDL_AUTO, "update");
